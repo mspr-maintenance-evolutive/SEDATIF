ae30a7105541b80a3b3200d345137eea414657db
Jules Peguet <julespeguet.jp@gmail.com>
2021-05-04 09:00:54 +0200

    add /espaces/travaux & /espaces/ouvert

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ app.use(morgan('common'))
 app.use(cors('*'))
 
 
-app.get('/', async (req, res) => res.send(sedatif.sedatif()))
+app.get('/', async (req, res) => {
+    res.send("Welcome on API SEDATIF")
+})
+
+app.get('/espaces/travaux', async (req, res) => {
+    res.header('Content-Type', 'text/csv');
+    res.attachment(`espace-en-travaux-${Date.now()}.csv`);
+    res.send(sedatif.espacesEnTravaux())
+})
+
+app.get('/espaces/ouvert', async (req, res) => {
+    res.header('Content-Type', 'text/csv');
+    res.attachment(`espace-ouvert-${Date.now()}.csv`);
+    res.send(sedatif.espacesOuverts())
+})
 
 const port = process.env.PORT || 5225
 const server = app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
